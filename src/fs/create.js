4d6f3a6eb279bffb7230fd2b72814921700d41b2
Tony <un_razum@mail.ru>
2023-06-19 23:30:21 +0300

    feat:: solving create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { writeFile } from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const data = "I am fresh and young";
+  try {
+    await writeFile("./src/fs/files/fresh.txt", data);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await create();
